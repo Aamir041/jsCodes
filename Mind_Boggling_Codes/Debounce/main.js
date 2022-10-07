ece943849c8629ec7849d20d813547d6b459b31c
Aamir Saudagar <76783873+Aamir041@users.noreply.github.com>
2022-10-07 15:32:30 +0530

    Update main.js

--- Mind_Boggling_Codes/Debounce/main.js
+++ Mind_Boggling_Codes/Debounce/main.js
@@ -1,4 +1,4 @@
-let counter = 0; 
+let counter = 0; // track karta hai kitne call kiye hai ab tak
 const getData = () =>{
     console.log(counter++," : Fetching data ...");
 };
@@ -6,7 +6,7 @@ const getData = () =>{
 const debounce = (callBack,delay) =>{
     let timer;
     return function (){
-        clearTimeout(timer)
+        clearTimeout(timer) // clear timeout agar user 300 ms ke beech me type karta hai pehle wale setTimeout ke baad.
         timer = setTimeout(() => {
             callBack();
         }, delay);
@@ -14,4 +14,4 @@ const debounce = (callBack,delay) =>{
     }
 }
 
-const betterFunction = debounce(getData,300) 
+const betterFunction = debounce(getData,300) // debounce ko call karta hai, callback is getData, delay to make API call is 300ms
