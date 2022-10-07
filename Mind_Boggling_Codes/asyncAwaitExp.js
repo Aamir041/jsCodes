219a8928d303220408ebbe737d45c16da1c2a081
Aamir Saudagar <76783873+Aamir041@users.noreply.github.com>
2022-10-07 10:36:49 +0530

    Create asyncAwaitExp.js

---
+++ Mind_Boggling_Codes/asyncAwaitExp.js
@@ -0,0 +1,28 @@
+const fakePromise = (userName,passWord) => new Promise((resolve,reject) => {
+    userName=="Aamir" && passWord =="a12" ? resolve("Accepted") : reject("N/A")
+})
+
+const assignRole = (status) => new Promise( (resolve) => resolve("Manager"))
+
+const assignTime = () => new Promise((resolve) => {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    resolve(time)
+})
+
+async function logIn (userName,passWord){
+    try{
+        const fakeResponse = await fakePromise(userName,passWord)
+        console.log("User authenticated wait to see the role ...")
+        const takeRole = await assignRole(fakeResponse)
+        console.log(`${userName} is a ${takeRole}`)
+        const timeLogged = await assignTime()
+        console.log(`${userName} logged time is ${timeLogged}`)
+    } catch{
+        console.log("Credentials Dont Match")
+    }
+
+
+}
+logIn("Aamir Saudagar","a12")
+
