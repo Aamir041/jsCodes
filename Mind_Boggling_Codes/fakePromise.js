9346714ca863274f3b18a54172b6e43b233a4e95
Aamir041 <aamirsaudagar34@gmail.com>
2022-10-12 15:24:24 +0530

    changed name to important codes

--- Mind_Boggling_Codes/fakePromise.js
+++
@@ -1,9 +0,0 @@
-const fakePromise = (msg,shouldReject) => new Promise((resolve,reject) =>{
-    shouldReject ? reject(`WTH is this ungodly message: ${msg}`):resolve("Wow Your Message Passed")
-})
-
-const err = (error) => console.log(error)
-
-fakePromise("Your Dumb",true).then(
-    (data) => console.log(data)
-    ).catch(err)
--- Mind_Boggling_Codes/guessTheOputForJoin.js
+++
@@ -1,2 +0,0 @@
-console.log([1, , 3].join()); // '1,,3'
-console.log([1, undefined, 3].join()); // '1,,3' 
